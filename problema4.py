67d95610dc4c352531c0f711d413a44d91e0efed
Gerardo-HG <157294307+Gerardo-HG@users.noreply.github.com>
2024-01-25 05:07:02 +0000

    Subiendo ejercicio 4

---
+++ problema4.py
@@ -0,0 +1,4 @@
+numero=int(input("Ingrese un numero entero: "))
+suma_de_numeros=numero*(numero+1)/2
+print("\n")
+print("La suma de los {} primeros enteros positivos es {}".format(numero,suma_de_numeros)) 
