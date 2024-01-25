4d8fcf26b3015dd8cd2197d93d5171213dd8d429
Gerardo-HG <157294307+Gerardo-HG@users.noreply.github.com>
2024-01-25 05:06:34 +0000

    Subiendo ejercicio 3

---
+++ problema3.py
@@ -0,0 +1,8 @@
+n_payasos=int(input("Ingrese la cantidad de payasos vendidos en el último pedido: "))
+n_muñecas=int(input("Ingrese la cantidad de muñecas vendidas en el último pedido: "))
+peso_payaso=112
+peso_muñeca=75
+print("\n")
+print("El peso total del paquete que será enviado es {} g.".format((peso_payaso*n_payasos)+(peso_muñeca*n_muñecas)))
+ 
+ 
